656674246447641212890d071ba2aa8e09d44fb9
matelakjakub <73526507+matelakjakub@users.noreply.github.com>
2023-03-21 22:30:36 +0100

    Update TodoItem.jsx
    after update / part 5+6

--- projects/react-todo-app/src/components/TodoItem.jsx
+++ projects/react-todo-app/src/components/TodoItem.jsx
@@ -1,4 +1,10 @@
-const TodoItem = ({itemProp}) => {
-    return <li>{itemProp.title}</li>
+const TodoItem = ({ itemProp, handleChange, delTodo }) => {
+	return (
+		<li>
+			<button onClick={() => delTodo(itemProp.id)}>Delete</button>
+			<input type='checkbox' checked={itemProp.completed} onChange={() => handleChange(itemProp.id)} />
+			{itemProp.title}
+		</li>
+	)
 }
-export default TodoItem;
+export default TodoItem
